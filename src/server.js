cc782a02869d3311f3aeed76dfbd1ab3bc71a92a
insidelamp <rkdhfh@naver.com>
2022-05-16 17:44:35 +0900

    frontend 연결

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/public/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (reg, res) => res.render("home"));
+const handleListen = () => console.log("Listening on http:/localhost:3000");
 
-app.listen(3000);
+app.listen(3000, handleListen);
