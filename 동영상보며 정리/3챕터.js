da3bed7882dd83482b61ed8a6e3e3737c751d5e7
insidelamp <rkdhfh@naver.com>
2022-05-16 19:54:32 +0900

    웹소켓 서버연결

---
+++ 동영상보며 정리/3챕터.js
@@ -0,0 +1,55 @@
+/*
+
+1. Protocol  - 사람들이 방에 모여 표준이되는 규칙
+
+    1. 어떤 사람들이 어딘가에 있는 방에서 만나고 그리고 어떻게 일들이 진행될지를 결정함
+    2. 어떻게 모든 것이 돌아가야 할지에 대한 규칙을 만듬
+
+
+그 다음 프로그래머는 이 규칙을 가지고 규칙을 따르는 코드를 만들어서 실행함 
+개발자들이 이 규칙을 코드에 녹여내어 만드는것 
+*/
+
+/*
+
+2. Ws : a Node.js WebSocket library - ws는 webSocket 
+    1. ws는 가장 기초적이고 중심적이며 핵심적인 언어임
+    2. 부가적인 유틸리티및 부가 기능이 없음
+    3. Express는 ws를 지원안함  function 을 사용해야함
+
+
+*/
+
+/*
+
+3.  http 서버가 있으면 그 위에서 ws서버를 만들수있음
+    1. Const server = http.createServer(app); // Listening on http:/localhost:3000
+    2. Const was = http.createServer({server}); // Listening on ws:/localhost:3000
+    3.  2개의 protocol 다 같은 port를 공유함
+    4. 항상 이렇게 만들 필요없이 ws 만 만들어도되고 http 만 만들어도됨
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
+
+/*
+
+*/
